0b7dc3862695733e4b5c7710b668c619e0cfc4fc
Aidan <omaraa@utas.edu.au>
2017-05-10 21:16:01 +1000

    Added "About ImageSURF" command.

---
+++ src/main/java/imagesurf/AboutImageSurf.java
@@ -0,0 +1,37 @@
+package imagesurf;
+
+import net.imagej.ImageJ;
+import org.scijava.ItemIO;
+import org.scijava.command.Command;
+import org.scijava.plugin.Attr;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+@Plugin(type = Command.class, menuPath = "Plugins>Segmentation>ImageSURF>About ImageSURF",
+		headless = true)
+public class AboutImageSurf implements Command {
+
+	@Parameter(type = ItemIO.OUTPUT)
+	private String ImageSurf;
+
+	@Override
+	public void run() {
+		ImageSurf = "ABOUT IMAGESURF\n\n" +
+				"ImageSURF (Image Segmentation Using Random Forests) is an ImageJ2 plugin and Java API for pixel-based " +
+				"image segmentation that uses an optimised implementation of the random forests machine learning " +
+				"algorithm and a set of selected image features." +
+				"\n\n" +
+				"For instructions on how to use ImageSURF visit https://github.com/omaraa/ImageSURF/" +
+				"\n\n" +
+				"To get help, report bugs or request features visit the ImageSURF GitHub issues page at "+
+				"https://github.com/omaraa/ImageSURF/issues";
+	}
+
+	public static void main(String[] args)
+	{
+		// create the ImageJ application context with all available services
+		final ImageJ ij = net.imagej.Main.launch(args);
+
+		ij.command().run(AboutImageSurf.class, true);
+	}
+}
